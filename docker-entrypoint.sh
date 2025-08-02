8364ea76b0ff47314cc0db3de19df6b389881e69
kiyo9w <ngokapikapi@gmail.com>
2025-08-02 12:58:01 +0700

    yeah

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,11 @@ set -e
 
 # Run migrations
 echo "--- Running database migrations ---"
-alembic upgrade head
+cd /app/backend && alembic upgrade head
 
 # Create initial data in database
 echo "--- Creating initial data ---"
-python /app/app/initial_data.py
+cd /app/backend && python app/initial_data.py
 
 # Start the application server
 echo "--- Starting Uvicorn server ---"
